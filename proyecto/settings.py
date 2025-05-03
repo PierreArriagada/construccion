7f11ad83fd3beeb122a0325a40bd081a0e71f6d1
Pierre <sotopierre@gmail.com>
2025-05-02 21:38:09 -0400

    actualizacion

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -80,9 +80,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'cloudinary',
     'cloudinary_storage',
+    'django.contrib.staticfiles',
     'construccion',
 ]
 
