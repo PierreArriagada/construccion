7a03bf660311c2cfd6c1d08343952bd19e0a833c
Pierre <sotopierre@gmail.com>
2025-05-02 21:40:13 -0400

    upto

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -79,9 +79,9 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',
-    'cloudinary',
+        'cloudinary',
     'cloudinary_storage',
+    'django.contrib.messages',
     'django.contrib.staticfiles',
     'construccion',
 ]
